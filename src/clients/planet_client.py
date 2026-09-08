30d9ff4e0cbd4809b1977f94f9df8ce1b26de0b3
Меирман <meirman@MacBook-Air-Meirman.local>
2026-09-08 22:29:57 +0500

    Исправлены ошибки в клиенте Планета

--- src/clients/planet_client.py
+++ src/clients/planet_client.py
@@ -6,10 +6,10 @@ from src.clients.base_client import BaseApiClient
 class PlanetClient(BaseApiClient):
     PLANET = 'people'
 
-    def get_all_persons(self) -> requests.Response:
-        response = self.session.get(self._url(self.PERSON))
+    def get_all_planets(self) -> requests.Response:
+        response = self.session.get(self._url(self.PLANET))
         return response
 
-    def get_person_by_id(self, person_id) -> requests.Response:
-        response = self.session.get(self._url(self.PERSON + f'/{person_id}'))
+    def get_planet_by_id(self, planet_id) -> requests.Response:
+        response = self.session.get(self._url(self.PLANET + f'/{planet_id}'))
         return response
